07fb09137ecdf9c4771a0aae2c5160d9731e97ac
Natalia Podosinnikova <Natalia@Natalias-MacBook-Air.local>
2016-02-18 21:17:01 -0600

    all tasks complete

--- main.js
+++ main.js
@@ -44,3 +44,36 @@ var addItem = function(keyEvent) {
 inputBar.addEventListener('keydown',addItem)
 
 // TASK 3: BONUS
+
+var inputEl = document.querySelector("#add-guest-bonus input")
+
+var guestList = document.querySelector("#add-guest-bonus ul")
+
+var removeGuest = function() {
+	this.parentNode.parentNode.removeChild(this.parentNode)
+} 
+
+var addItem = function(keyEvent) {
+  var inputEl = keyEvent.srcElement
+  if (keyEvent.keyCode === 13) {
+      var userInput = inputEl.value
+      var newLi = document.createElement('li')
+      	newLi.style.display = "hidden"
+      var redButton = document.createElement("button")
+			redButton.innerHTML = "X"
+			redButton.addEventListener("click", removeGuest)
+      newLi.textContent = userInput
+      guestList.appendChild(newLi)
+	  newLi.classList.add("guest")
+	  newLi.appendChild(redButton)
+      inputEl.value = ''
+  	} 
+  }
+  
+inputEl.addEventListener('keydown',addItem)
+
+
+
+
+
+
