6392f1dd414992d63a08b692929c2a2671800942
Natalia Podosinnikova <Natalia@Natalias-MacBook-Air.local>
2016-02-19 10:51:16 -0600

    addnl methods

--- main.js
+++ main.js
@@ -49,19 +49,32 @@ var inputEl = document.querySelector("#add-guest-bonus input")
 
 var guestList = document.querySelector("#add-guest-bonus ul")
 
-var removeGuest = function() {
-	this.parentNode.parentNode.removeChild(this.parentNode)
-} 
+// var removeGuest = function() {
+// 	this.parentNode.parentNode.removeChild(this.parentNode)
+// } 
+
+
 
 var addItem = function(keyEvent) {
   var inputEl = keyEvent.srcElement
   if (keyEvent.keyCode === 13) {
       var userInput = inputEl.value
       var newLi = document.createElement('li')
-      	newLi.style.display = "hidden"
       var redButton = document.createElement("button")
 			redButton.innerHTML = "X"
-			redButton.addEventListener("click", removeGuest)
+      // var boundFunction = removeGuest.bind(redButton)  <= browser binding act
+      // boundFunction()
+			//redButton.addEventListener("click", removeGuest)
+      var remover = function() {
+        guestList.removeChild(newLi)
+      }
+      redButton.addEventListener("click",remover)
+      // var remover2 = function(eventObj) {
+      //   var removeButton = eventObj.srcElement
+      //   var li = removeButton.parentNode
+      //   var ul = li.parentNode
+      //   ul.removeChild(li)
+      // }
       newLi.textContent = userInput
       guestList.appendChild(newLi)
 	  newLi.classList.add("guest")
